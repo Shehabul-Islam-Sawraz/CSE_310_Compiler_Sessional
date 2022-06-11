516a84f8b1effca970c027fe17b27f582f06b26a
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-06-11 18:15:53 +0600

    Keywords part done

--- Lexical Analyzer/LexicalAnalyzer.h
+++ Lexical Analyzer/LexicalAnalyzer.h
@@ -1,6 +1,34 @@
 #include "SymbolTable.h"
+#include<cstring>
 
 FILE *logout, *tokenout;
 
 int line_count = 1;
 int word_count = 0;
+
+string toUpper(string token){
+    string temp = "";
+    int len  = token.length();
+    for(int i=0;i<len;i++){
+        temp+=toupper(token[i]);
+    }
+    return temp;
+}
+
+void printLogData(int noOfLine, string token){
+    fprintf(logout,"Line no %d: TOKEN <%s> Lexeme %s found\n",noOfLine, token.data(), yytext);
+}
+
+void printToken(string token){
+    fprintf(tokenout, "<%s>", token.data());
+    printLogData(line_count, token);
+}
+
+void printTokenWithSymbol(string token){
+    fprintf(tokenout, "<%s, %s>", token.data(), yytext);
+    printLogData(line_count, token);
+}
+
+void addKeywords(){
+    printToken(toUpper(yytext));
+}
