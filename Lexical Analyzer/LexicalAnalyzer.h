e1e29ee52df2fbdd3ca06768c3ae910afd8a6e87
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-06-12 23:39:56 +0600

    String handled

--- Lexical Analyzer/LexicalAnalyzer.h
+++ Lexical Analyzer/LexicalAnalyzer.h
@@ -5,6 +5,8 @@
 
 int line_count = 1;
 int word_count = 0;
+string str="",original_str="";
+int string_line_no;
 
 ScopeTable* scope = nullptr;
 SymbolTable* symbolTable = new SymbolTable();
@@ -42,10 +44,10 @@ void printTokenWithSymbol(string token){
     printLogData(line_count, token);
 }
 
-void printTokenWithSymbol(string token, string lexeme){
-    fprintf(tokenout, "<%s, %s>", token.data(), lexeme.data());
-    printLogData(line_count, token);
-}
+// void printTokenWithSymbol(string token, string lexeme){
+//     fprintf(tokenout, "<%s, %s>", token.data(), lexeme.data());
+//     printLogData(line_count, token);
+// }
 
 void addKeywords(){
     printToken(toUpper(yytext));
@@ -81,4 +83,46 @@ void addOperators(string token){
         scope = symbolTable->createScopeTable(NoOfBuckets);
     }
     printTokenWithSymbol(token);
+}
+
+void addString(string token, string str){
+    fprintf(tokenout, "<%s, %s>", token.data(), yytext);
+    fprintf(logout,"Line no %d: TOKEN <%s> Lexeme %s found --> < %s, %s >\n",string_line_no, token.data(), str, token.data(), yytext);
+}
+
+void handleSpecialStringCharacters(){
+    original_str.append(yytext);
+    if(yytext[1] == '\\'){
+        str.append(1u, '\\');
+    }
+    if(yytext[1] == 'n'){
+		str.append(1u, '\n');
+    }
+    if(yytext[1] == 't'){
+        str.append(1u, '\t');
+    }
+    if(yytext[1] == '\''){
+        str.append(1u, '\'');
+    }
+    if(yytext[1] == 'a'){
+        str.append(1u, '\a');
+    }
+    if(yytext[1] == 'f'){
+        str.append(1u, '\f');
+    }
+    if(yytext[1] == 'r'){
+        str.append(1u, '\r');
+    }
+    if(yytext[1] == 'b'){
+        str.append(1u, '\b');
+    }
+    if(yytext[1] == 'v'){
+        str.append(1u, '\v');
+    }
+    if(yytext[1] == '0'){
+        str.append(1u, '\0');
+    }	
+    if(yytext[1] == '\"'){
+        str.append(1u, '\"');
+    }
 }
