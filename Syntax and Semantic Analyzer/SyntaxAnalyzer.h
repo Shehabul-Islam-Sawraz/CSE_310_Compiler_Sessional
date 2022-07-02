1ce0f07c13f96737769524d59209c40393a74779
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-07-03 01:41:31 +0600

    Bison code for detecting type_specifier added

--- Syntax and Semantic Analyzer/SyntaxAnalyzer.h
+++ Syntax and Semantic Analyzer/SyntaxAnalyzer.h
@@ -21,6 +21,12 @@ SymbolTable* symbolTable = new SymbolTable();
 int yyparse();
 int yylex();
 
+string ruleName[] = {"start", "program", "unit", "func_declaration", "func_definition", "parameter_list",
+                           "compound_statement", "var_declaration", "type_specifier", "declaration_list", "statements",
+                           "statement", "expression_statement", "variable", "expression", "logic_expression",
+                           "rel_expression", "simple_expression", "term", "unary_expression", "factor", "argument_list",
+                           "arguments"};
+
 enum NONTERMINAL_TYPE {
 	start = 0, program, unit, func_declaration, func_definition,
 	parameter_list, compound_statement, var_declaration, type_specifier,
@@ -29,7 +35,74 @@ enum NONTERMINAL_TYPE {
 	term, unary_expression, factor, argument_list, arguments, error
 };
 
+bool replaceAll(string &source, string &toReplace, string &replaceBy) {
+	if (source.find(toReplace, 0) == string::npos){
+		return false;
+	}
+	for (string::size_type i = 0; (i = source.find(toReplace, i)) != string::npos;) {
+		source.replace(i, toReplace.length(), replaceBy);
+		i += replaceBy.length();
+	}
+	return true;
+}
+
+string formatCode(string &code){
+	string formattedCode = code;
+	while (replaceAll(formattedCode, " ;", ";"));
+	replaceAll(formattedCode, ";", ";\n");
+	replaceAll(formattedCode, ";\n\n", ";\n");
+	while (replaceAll(formattedCode, "( ", "("));
+	while (replaceAll(formattedCode, " (", "("));
+	while (replaceAll(formattedCode, ") ", ")"));
+	while (replaceAll(formattedCode, " )", ")"));
+	while (replaceAll(formattedCode, "{ ", "{"));
+	while (replaceAll(formattedCode, " {", "{"));
+	while (replaceAll(formattedCode, "} ", "}"));
+	while (replaceAll(formattedCode, " }", "}"));
+	replaceAll(formattedCode, "{", "\n{\n");
+	replaceAll(formattedCode, "}", "\n}\n");
+	while (replaceAll(formattedCode, "\n\n}", "\n}"));
+	while (replaceAll(formattedCode, "}\n\n", "}\n"));
+	while (replaceAll(formattedCode, "  = ", " = "));
+	while (replaceAll(formattedCode, " =  ", " = "));
+	while (replaceAll(formattedCode, "  == ", " == "));
+	while (replaceAll(formattedCode, " ==  ", " == "));
+	replaceAll(formattedCode, "(", " ( ");
+	replaceAll(formattedCode, ")", " ) ");
+	while (replaceAll(formattedCode, " ++", "++"));
+	while (replaceAll(formattedCode, " --", "--"));
+	while (replaceAll(formattedCode, "\n ", "\n"));
+	while (replaceAll(formattedCode, " \n", "\n"));
+	while (replaceAll(formattedCode, "  ", " "));
+	return formattedCode;
+}
+
+class NonTerminalHandler
+{
+private:
+	stack<string> nonterminals[NONTERMINAL_TYPE::error +1];
+public:
+	string getValue(NONTERMINAL_TYPE nonterminal){
+		return nonterminals[nonterminal].top();
+	}
+	void setValue(NONTERMINAL_TYPE nonterminal, string value){
+		nonterminals[nonterminal].push(value);
+	}
+};
+
+NonTerminalHandler nonTerminalHandler = new NonTerminalHandler();
+
 SymbolInfo* getSymbolInfoOfType(string type){
 	variableType = type;
 	return new SymbolInfo(variableType,variableType);
 }
+
+void setValue(NONTERMINAL_TYPE nonterminal, string value){
+	nonTerminalHandler.setValue(nonterminal,value);
+}
+
+void printRuleAndCode(NONTERMINAL_TYPE nonterminal, string rule){
+	fprintf(logout,"Line %d: %s : %s\n",line_count, rulename.data(), rule.data());
+	fprintf(logout,"%s\n", formatCode(nonTerminalHandler.getValue(nonterminal)).data());
+}
+
--- Syntax and Semantic Analyzer/SyntaxAnalyzer.y
+++ Syntax and Semantic Analyzer/SyntaxAnalyzer.y
@@ -4,6 +4,7 @@
 
 %union{
     SymbolInfo* symbolInfo;
+    vector<SymbolInfo*>* vectorsymbol;
 }
 
 %token IF ELSE SWITCH CASE DEFAULT FOR DO WHILE INT FLOAT DOUBLE CHAR STRING VOID BREAK RETURN CONTINUE
@@ -23,14 +24,50 @@
 type_specifier : INT
                         {
                             $$ = getSymbolInfoOfType(INT_TYPE);
+                            setValue(type_specifier,"int");
+                            printRuleAndCode(type_specifier,"INT");
                         }
                 | FLOAT
                         {
                             $$ = getSymbolInfoOfType(FLOAT_TYPE);
+                            setValue(type_specifier,"float");
+                            printRuleAndCode(type_specifier,"FLOAT");
                         }
                 | VOID
                         {
                             $$ = getSymbolInfoOfType(VOID_TYPE);
+                            setValue(type_specifier,"void");
+                            printRuleAndCode(type_specifier,"VOID");
                         }
                 ;
 %%
+
+int main(int argc,char *argv[])
+{
+    if(argc!=2){
+		cout << "Please provide input file name" << endl;
+		return 0;
+	}
+	
+	FILE *inputFile=fopen(argv[1],"r");
+	if(inputFile==NULL){
+		cout << "Cannot open input file\n";
+		return 0;
+	}
+
+	logout.open("log.txt");
+	errorout.open("error.txt");
+	parserout.open("parser.txt");
+
+    yyin = inputFile;
+	yyparse();
+    line_count--;
+    fprintf(logout,"\nTotal Lines: %d\nTotal Errors: %d\n",line_count, error_count);
+
+    fclose(yyin);
+	logout.close();
+	errorout.close();
+    parserout.close();
+
+	return 0;
+}
